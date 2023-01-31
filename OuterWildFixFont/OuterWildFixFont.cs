009a19d5cb52ff852cde2b1154ed2a506f3a6060
nice2cu1 <nice2cu1@outlook.com>
2023-01-31 18:40:38 +0800

    Update

--- OuterWildFixFont/OuterWildFixFont.cs
+++ OuterWildFixFont/OuterWildFixFont.cs
@@ -27,25 +27,27 @@ namespace OuterWildFixFont
                     TMPTranslateFont = ab.LoadAsset<TMP_FontAsset>($"{fontName} SDF");
                     if (TranslateFont != null && TMPTranslateFont != null)
                     {
-                        
                     }
                     else
                     {
-                        ModHelper.Console.WriteLine("$The font file is damaged. Please check the file.", MessageType.Error);
+                        ModHelper.Console.WriteLine("$The font file is damaged. Please check the file.",
+                            MessageType.Error);
                     }
+
                     ab.Unload(false);
                 }
                 else
                 {
-                    ModHelper.Console.WriteLine($"Font {fontName} not found, Please check the path: {path}", MessageType.Error);
+                    ModHelper.Console.WriteLine($"Font {fontName} not found, Please check the path: {path}",
+                        MessageType.Error);
                 }
             }
             catch (Exception e)
             {
                 ModHelper.Console.WriteLine($"Load font exception:{e.Message}\n{e.StackTrace}", MessageType.Error);
-
             }
         }
+
         private void Awake()
         {
             // You won't be able to access OWML's mod helper in Awake.
@@ -59,9 +61,8 @@ namespace OuterWildFixFont
             ModHelper.Console.WriteLine($"My mod {nameof(OuterWildFixFont)} is loaded!", MessageType.Success);
             LoadFont("pingfang");
             Harmony.CreateAndPatchAll(typeof(OuterWildFixFont));
-            ModHelper.Console.WriteLine("###################RUN###################", MessageType.Success);
         }
-        
+
         //人物对话框
         [HarmonyPrefix, HarmonyPatch(typeof(DialogueBoxVer2), "InitializeFont")]
         public static bool InitializeFontPatch(DialogueBoxVer2 __instance)
@@ -84,6 +85,7 @@ namespace OuterWildFixFont
                 __instance._dynamicFontInUse = TextTranslation.GetFont(true);
                 __instance._fontSpacingInUse = TextTranslation.GetDefaultFontSpacing();
             }
+
             __instance._mainTextField.font = __instance._fontInUse;
             __instance._mainTextField.lineSpacing = __instance._fontSpacingInUse;
             __instance._nameTextField.font = __instance._fontInUse;
@@ -94,7 +96,7 @@ namespace OuterWildFixFont
 
             return false;
         }
-        
+
         //挪麦文字翻译器
         [HarmonyPrefix, HarmonyPatch(typeof(NomaiTranslatorProp), "InitializeFont")]
         public static bool InitializeFontPatch(NomaiTranslatorProp __instance)
@@ -110,12 +112,163 @@ namespace OuterWildFixFont
                 __instance._dynamicFontInUse = TextTranslation.GetFont(true);
                 __instance._fontSpacingInUse = TextTranslation.GetDefaultFontSpacing();
             }
+
             __instance._textField.font = __instance._fontInUse;
             __instance._textField.lineSpacing = __instance._fontSpacingInUse;
             return false;
         }
 
+         //FontAndLanguageController
+         [HarmonyPrefix, HarmonyPatch(typeof(FontAndLanguageController), "InitializeFont")]
+         public static bool InitializeFontPatch(FontAndLanguageController __instance)
+         {
+             bool flag = TextTranslation.Get().IsLanguageLatin();
+             Font languageFont = TranslateFont;
+             for (int i = 0; i < __instance._textContainerList.Count; i++)
+             {
+                 TextStyleApplier component =
+                     __instance._textContainerList[i].textElement.GetComponent<TextStyleApplier>();
+                 if (__instance._textContainerList[i].isLanguageFont)
+                 {
+                     if (__instance._textContainerList[i].originalFont == languageFont)
+                     {
+                         __instance._textContainerList[i].textElement.font = languageFont;
+                         __instance._textContainerList[i].textElement.lineSpacing =
+                             __instance._textContainerList[i].originalSpacing;
+                         __instance._textContainerList[i].textElement.fontSize =
+                             TextTranslation.GetModifiedFontSize(__instance._textContainerList[i].originalFontSize);
+                         __instance._textContainerList[i].textElement.rectTransform.localScale =
+                             __instance._textContainerList[i].originalScale;
+                     }
+                     else
+                     {
+                         int modifiedFontSize = TextTranslation.GetModifiedFontSize(languageFont.fontSize);
+                         __instance._textContainerList[i].textElement.font = languageFont;
+                         __instance._textContainerList[i].textElement.lineSpacing =
+                             TextTranslation.GetDefaultFontSpacing();
+                         if (__instance._textContainerList[i].shouldScale)
+                         {
+                             __instance._textContainerList[i].textElement.fontSize = modifiedFontSize;
+                             Vector3 vector = __instance._textContainerList[i].originalScale *
+                                              ((float)__instance._textContainerList[i].originalFontSize /
+                                               (float)modifiedFontSize);
+                             __instance._textContainerList[i].textElement.rectTransform.localScale = vector;
+                         }
+                         else
+                         {
+                             __instance._textContainerList[i].textElement.fontSize =
+                                 TextTranslation.GetModifiedFontSize(__instance._textContainerList[i].originalFontSize);
+                         }
         
+                         if (__instance._textContainerList[i].useDefaultLineSpacing)
+                         {
+                             __instance._textContainerList[i].textElement.lineSpacing =
+                                 TextTranslation.GetDefaultFontSpacing();
+                         }
+                         else
+                         {
+                             __instance._textContainerList[i].textElement.lineSpacing =
+                                 __instance._textContainerList[i].originalSpacing;
+                         }
+                     }
+                 }
+                 else if (flag)
+                 {
+                     __instance._textContainerList[i].textElement.font = languageFont;
+                     __instance._textContainerList[i].textElement.lineSpacing =
+                         __instance._textContainerList[i].originalSpacing;
+                     __instance._textContainerList[i].textElement.fontSize =
+                         __instance._textContainerList[i].originalFontSize;
+                     __instance._textContainerList[i].textElement.rectTransform.localScale =
+                         __instance._textContainerList[i].originalScale;
+                 }
+                 else
+                 {
+                     Font font = TextTranslation.GetFont(__instance._textContainerList[i].originalFont.dynamic);
+                     if (__instance._textContainerList[i].originalFont == font)
+                     {
+                         __instance._textContainerList[i].textElement.font = languageFont;
+                         __instance._textContainerList[i].textElement.lineSpacing =
+                             __instance._textContainerList[i].originalSpacing;
+                         __instance._textContainerList[i].textElement.fontSize =
+                             TextTranslation.GetModifiedFontSize(__instance._textContainerList[i].originalFontSize);
+                         __instance._textContainerList[i].textElement.rectTransform.localScale =
+                             __instance._textContainerList[i].originalScale;
+                     }
+                     else if (font.dynamic)
+                     {
+                         __instance._textContainerList[i].textElement.fontSize =
+                             TextTranslation.GetModifiedFontSize(__instance._textContainerList[i].originalFontSize);
+                         __instance._textContainerList[i].textElement.rectTransform.localScale =
+                             __instance._textContainerList[i].originalScale;
+                         __instance._textContainerList[i].textElement.font = languageFont;
+                         if (__instance._textContainerList[i].useDefaultLineSpacing)
+                         {
+                             __instance._textContainerList[i].textElement.lineSpacing =
+                                 TextTranslation.GetDefaultFontSpacing();
+                         }
+                         else
+                         {
+                             __instance._textContainerList[i].textElement.lineSpacing =
+                                 __instance._textContainerList[i].originalSpacing;
+                         }
+                     }
+                     else
+                     {
+                         int modifiedFontSize2 = TextTranslation.GetModifiedFontSize(font.fontSize);
+                         __instance._textContainerList[i].textElement.font = languageFont;
+                         __instance._textContainerList[i].textElement.lineSpacing =
+                             TextTranslation.GetDefaultFontSpacing();
+                         if (__instance._textContainerList[i].shouldScale)
+                         {
+                             __instance._textContainerList[i].textElement.fontSize = modifiedFontSize2;
+                             Vector3 vector2 = __instance._textContainerList[i].originalScale *
+                                               ((float)__instance._textContainerList[i].originalFontSize /
+                                                (float)modifiedFontSize2);
+                             __instance._textContainerList[i].textElement.rectTransform.localScale = vector2;
+                         }
+                         else
+                         {
+                             __instance._textContainerList[i].textElement.fontSize =
+                                 TextTranslation.GetModifiedFontSize(__instance._textContainerList[i].originalFontSize);
+                         }
+        
+                         if (__instance._textContainerList[i].useDefaultLineSpacing)
+                         {
+                             __instance._textContainerList[i].textElement.lineSpacing =
+                                 TextTranslation.GetDefaultFontSpacing();
+                         }
+                         else
+                         {
+                             __instance._textContainerList[i].textElement.lineSpacing =
+                                 __instance._textContainerList[i].originalSpacing;
+                         }
+                     }
+                 }
+        
+                 if (component != null)
+                 {
+                     component.font = languageFont;
+                     if (!TextTranslation.Get().IsLanguageLatin() &&
+                         TextTranslation.Get().GetLanguage() != TextTranslation.Language.RUSSIAN &&
+                         TextTranslation.Get().GetLanguage() != TextTranslation.Language.POLISH &&
+                         TextTranslation.Get().GetLanguage() != TextTranslation.Language.TURKISH)
+                     {
+                         component.fixedWidth = (float)__instance._textContainerList[i].textElement.font.fontSize;
+                     }
+                     else
+                     {
+                         component.fixedWidth = 0f;
+                     }
+                 }
+        
+                 __instance._textContainerList[i].textElement.SetAllDirty();
+             }
+        
+             return false;
+         }
+
+
         // [HarmonyPostfix, HarmonyPatch(typeof(Text), "OnEnable")]
         // public static void FontPatch(Text __instance)
         // {
@@ -158,5 +311,4 @@ namespace OuterWildFixFont
         //     }
         // }
     }
-
 }
